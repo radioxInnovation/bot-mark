cf942c607b1e733ec3d5077a714da4c48d302a13
frettig-radiox <f.rettig@radiox-innovation.de>
2025-08-19 07:54:24 +0200

    Bump version to 1.1.5

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ extras["all"] = [
 
 setuptools.setup(
     name="botmark",
-    version="1.1.4",
+    version="1.1.5",
     author="Frank Rettig",
     author_email="118481987+frettig-radiox@users.noreply.github.com",
     description="BotMark – Define, run, and document LLM chatbots in plain Markdown. Framework for executable, portable, and LLM-agnostic chatbot workflows.",
